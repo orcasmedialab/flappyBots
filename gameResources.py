afcc9364a1c7c4de1452c4d92e4b40ca6663d3cb
Jason <jason@orcasmedialab.com>
2024-08-15 01:18:40 -0700

    Overhauled Bird class to accomodate bot control. Removed button class. Added game images.

--- gameResources.py
+++ gameResources.py
@@ -2,55 +2,86 @@ import pygame
 from pygame.locals import *
 import random
 
+#Global Environment Definitions
+pipe_gap = 150
+pipe_frequency = 1500 #ms
+groundHeight = 768
+#Global Player Definitions
+startHeight = int(groundHeight / 2)
+startDist = 100
+terminalVel = 8
+birdAccel = 0.5
+jumpSpeed = -8
+flapCooldown = 5
+rotateFactor = -3
+#define Game variables
+groundScroll = 0 #move to environment init
+scrollSpeed = 4
+gameover = False #move to game controller init
+lastPipe = pygame.time.get_ticks() - pipe_frequency
+passPipe = False
+
+
 class Bird(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
-        self.index = 0
-        self.counter = 0
         for num in range (1,4):
             img = pygame.image.load(f'img/bird{num}.png')
             self.images.append(img)
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
+        self.faceDown = -90
+        self.reset()
+
+    def reset(self):
+        self.index = 0
+        self.counter = 0
+        self.rect.center = [startDist, startHeight]
         self.vel = 0
-        self.clicked = False
+        self.score = 0
+        self.birdAlive = True
 
-    def update(self):
-
+    def updatePhysics(self):
         #gravity
-        if flying == True:
-            if self.vel < 8:
-                self.vel += 0.5
-            if self.rect.bottom < 768:
-                self.rect.y += int(self.vel)
+        if self.rect.bottom < groundHeight:
+            if self.vel < terminalVel:
+                self.vel += birdAccel
+            self.rect.y += int(self.vel)
 
-        if gameover == False:
-            #jump
-            if (((pygame.mouse.get_pressed()[0] == 1) or (pygame.key.get_pressed()[pygame.K_SPACE] == 1)) 
-                    and self.clicked == False):
-                self.clicked = True
-                self.vel = -8
-            if ((pygame.mouse.get_pressed()[0] == 0) and (pygame.key.get_pressed()[pygame.K_SPACE] == 0)):
-                self.clicked = False
+    def jump(self, jumpAction):
+        if jumpAction:
+            self.vel = jumpSpeed
 
-            #handle animation
-            self.counter += 1
-            flap_cooldown = 5
-
-            if self.counter > flap_cooldown:
+    def animateBird(self):
+        if self.birdAlive:
+            #flapping animation
+            if self.counter > flapCooldown:
                 self.counter = 0
                 self.index += 1
                 if self.index >= len(self.images):
                     self.index = 0
             self.image = self.images[self.index]
+            self.counter += 1
 
             #rotate the bird
             self.image = pygame.transform.rotate(self.images[self.index], 
-                    (-3 * self.vel))
+                    (rotateFactor * self.vel))
         else:
-            self.image = pygame.transform.rotate(self.images[self.index], -90)
+            #make dead bird face down
+            self.image = pygame.transform.rotate(self.images[self.index], self.faceDown)
+            #bird moves backwards with ground
+            self.rect.x -= int(scrollSpeed)
+
+    def updateBird(self, jumpAction):
+        self.updatePhysics()
+        self.jump(jumpAction)
+        self.animateBird()
+
+    def killBird(self):
+        self.birdAlive = False
+
+
 
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, x, y, position):
@@ -70,27 +101,4 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
-class Button():
-    def __init__(self, x, y, image):
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-
-    def draw(self):
-        action = False
-
-        #get mouse position
-        pos = pygame.mouse.get_pos()
-        #check if mouse is over button
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1:
-                action = True      
-        # Fix this to allow spacebar reset
-        #while (pygame.key.get_pressed()[pygame.K_SPACE] == 1):
-        #    action = True
-
-        #draw button
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-
-        return action
 
