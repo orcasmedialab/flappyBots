d2df1ead04474e2e3455ac82e2d5030883948ed7
Jason <jason@orcasmedialab.com>
2024-09-19 21:35:16 -0700

    Minor optimization to botController that removes redundant re-initialization of numpy arrays every step of game.

--- botController.py
+++ botController.py
@@ -13,34 +13,32 @@ class botController():
         self.numBirds = numBirds
         self.currentState = dict()
         self.botAlgorithm = ba.botAlgorithm(self.numBirds, numInputs)
+        self.jumpInstructions = np.zeros(self.numBirds)
+        self.params = np.empty([self.numBirds, numInputs])
+        self.inputs = np.zeros((2, numInputs))
 
     def compileParams(self):
-        params = np.empty([self.numBirds, numInputs])
-        params[:, 0] = self.currentState['pipeX']
-        params[:, 1] = self.currentState['pipeY']
-        params[:, 2] = self.currentState['birdY']
-        params[:, 3] = self.currentState['birdVel']
-
-        return params
+        self.params[:, 0] = self.currentState['pipeX']
+        self.params[:, 1] = self.currentState['pipeY']
+        self.params[:, 2] = self.currentState['birdY']
+        self.params[:, 3] = self.currentState['birdVel']
 
 
     def getInstructions(self, gameState):
         self.currentState = gameState
-        jumpInstructions = np.zeros(self.numBirds) #Optimize numpy initializations
 
         if self.currentState['gameOver'] == False:
-            inputs = np.zeros((2, numInputs)) #Optimize numpy initializations
-            params = self.compileParams()
+            self.compileParams()
             #print('Game Parameters:\n', params)
 
-            for inputIndex in range(params.shape[0]): 
-                if np.isnan(params[inputIndex, -1]) == False:
+            for inputIndex in range(self.numBirds): 
+                if np.isnan(self.params[inputIndex, -1]) == False:
                     #dumb solutions to resolve single-dimension bug in nn.py:308
-                    inputs[0, :] = params[inputIndex, :]
-                    instruction = self.botAlgorithm.calculateInstruction(inputs, inputIndex)
-                    jumpInstructions[inputIndex] = round(instruction[0].item())
+                    self.inputs[0, :] = self.params[inputIndex, :]
+                    instruction = self.botAlgorithm.calculateInstruction(self.inputs, inputIndex)
+                    self.jumpInstructions[inputIndex] = round(instruction[0].item())
                     #print('Jump Instructions:\n', jumpInstructions)
         
-        return jumpInstructions
+        return self.jumpInstructions
 
 
